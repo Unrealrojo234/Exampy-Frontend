68fbcb9d7c4f182749efce8c9e71b3283a44df24
Unrealrojo234 <otienoryan812@gmail.com>
2024-08-25 17:50:09 +0300

    Major Update

--- src/assets/Mocks/Mock.jsx
+++ src/assets/Mocks/Mock.jsx
@@ -1,58 +1,44 @@
-import { useState,useEffect } from "react";
-
+import { useState, useEffect } from "react";
 
 import BackToHome from "../keyAssets/BackToHome";
 
 const mockApi = import.meta.env.VITE_REACT_API_MOCK;
-export default function Mock(){
-    const[mocks,setMocks] = useState([]);
-    
-    useEffect(()=>{
-        fetch(mockApi)
-            .then(response=>response.json())
-            .then(data=>{
-                console.log(data)
-                setMocks(mocks=>data)
-            })
-            .catch(error=>console.log('Error', error));
-    },[])
+export default function Mock() {
+  const [mocks, setMocks] = useState([]);
 
-    const filePath = "../mockFilesPath";
+  useEffect(() => {
+    fetch(mockApi)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMocks((mocks) => data);
+      })
+      .catch((error) => console.log("Error", error));
+  }, []);
 
-    
-    //const base64Data = mocks.map((mock)=>mock.examfile); // Your Base64 encoded data
-    //const binaryData = Buffer.from(base64Data, 'base64');
-
-
-    //const saveToFile = (binaryData, filePath) => {
-    //fs.writeFile(filePath, binaryData, (err) => {
-      //  if (err) throw err;
-     //   console.log('File has been saved!');
-    //});
-    //}
-
-    //saveToFile()
-
-
-
-    return(
-        <div className="container-fluid">
-            <h1  className="text-center text-primary">Past Mock Exams for Candidates</h1>
-            <div>
-                <BackToHome/>
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center text-primary">
+        Past Mock Exams for Candidates
+      </h1>
+      <div>
+        {mocks.map((mock) => (
+          <div key={mock._id}>
+            <div className="card" style={{padding:'10px'}}>
+              <h5>{mock.school}</h5>
+              <span>{mock.name}</span>
+              <a style={{textDecoration:'none',display:'inline-block'}} href={mock.exampdf} download={mock.name}>
+                Download
+              </a>
             </div>
-            <div>
-                {mocks.map((mock)=>(
-                    <div>
-                        <ul>
-                            <li>
-                                <h5>{mock.school}</h5>
-                                <a download={"dwd"} href={mock.examfile}>Download</a>
-                            </li>
-                        </ul>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+          <br/>
+          </div>
+        ))}
+      </div>
+      <br />
+      <div>
+        <BackToHome />
+      </div>
+    </div>
+  );
 }
--- src/assets/keyAssets/BackToHome.jsx
+++ src/assets/keyAssets/BackToHome.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function BackToHome (){
     return(
         <div className="container-fluid fixed-bottom p-2  text-center">
-            <Link style={{textDecoration:'none'}} to={'/'}> <i className="fa-solid fa-house-chimney fa-2xl"></i> Back to Home Page</Link>
+            <Link style={{textDecoration:'none',display:'inline-block'}} to={'/'}> <i className="fa-solid fa-house-chimney fa-2xl"></i></Link>
         </div>
     )
 }
